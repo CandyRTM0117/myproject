a74efcd6f291fb829a36139ca2b55201e53d776b
Luvsandamba <luvsandamba@Luvsandambas-MacBook-Air.local>
2024-02-11 18:01:39 +0800

    drawererror

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,58 +1,36 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Stack } from "expo-router";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createStackNavigator } from "@react-navigation/stack";
+import TabOneScreen from "../app/(tabs)/index"
+import TabTwoScreen from "../app/(tabs)/index"
+import { NavigationContainer } from "@react-navigation/native";
 
-import { useColorScheme } from '@/components/useColorScheme';
+const Tab = createBottomTabNavigator();
 
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
+const HomeStack = createStackNavigator();
 
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+const Drawer = createDrawerNavigator();
+
+const Tabs = () => {
+  return (
+    <Tab.Navigator screenOptions={{ tabBarActiveTintColor: "red", headerShown: false, tabBarItemStyle: { marginBottom: -30 } }}>
+      <Tab.Screen
+        name="HomeScreen"
+        component={TabOneScreen}
+        options={{ tabBarShowLabel: false }}
+      />
+      <Tab.Screen name="About" component={TabTwoScreen} options={{ tabBarShowLabel: false }} />
+    </Tab.Navigator>
+  );
 };
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
-
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return <RootLayoutNav />;
-}
-
-function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
+export default function RootLayoutNav() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
+    <NavigationContainer>
+      <Drawer.Navigator screenOptions={{headerShown: false}}>
+        <Drawer.Screen name="Tabs" component={Tabs} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
